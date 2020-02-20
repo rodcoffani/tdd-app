4bc92453c373b07c6bea322e6f04bfa3030aa865
rodcoffani <rodrigo.coffani@gmail.com>
2020-02-20 16:51:56 -0300

    start tests

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
+const {
+    User
+} = require('../../src/app/models');
+
+describe('Authentication', () => {
+    it('Should authenticate with valid credentials', async () => {
+        const user = await User.create({
+           name: 'Rodrigo',
+           email: 'rodrigo.coffani@gmail.com',
+           password_hash: '123123'
+        });
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: 123456
+            });
+        
+        expect(response.status).toBe(200);
+    });
+});
