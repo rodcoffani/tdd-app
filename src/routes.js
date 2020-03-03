c8b536c93bc157e7d850c172b0c115a96817c7e4
rodcoffani <rodrigo.coffani@gmail.com>
2020-03-03 01:53:55 -0300

    authentication by token

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,14 @@
 const routes = require('express').Router();
+
 const SessionController = require('./app/controllers/SessionController');
+const authMiddleware = require('./app/middleware/auth');
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);  
+
+routes.get('/dashboard', (req, res) => {
+    return res.status(200).send();
+;})
 
 module.exports = routes;
